966982d113b1ad707f335d9c1df68582781d398a
Matheus <matheusgomes192@hotmail.com>
2020-09-13 17:01:34 -0300

    adicionando imagens de filtro de pequisas e fazendo sua exibição na Home

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -6,20 +6,35 @@ import {
 
     Scroll,
 
+    AdressView,     // Na verdade é um btn
+    AdressText,
+
     ScrollHorizontal,
 
     BtnFilter,
     BtnText,
+
+    ScrollImages,
+    ImgView,
+    Img,
+    ImgTitle,
 } from './style';
 
 export default () => {
     return(
         <Container>
             <Scroll>
+
+                <AdressView>
+                    <>
+                        <AdressText>R. Santos Dumont, 3008</AdressText>
+                        <Icon style={{marginLeft: 10}} name="angle-down" size={18} color="#ea1d2c"/>
+                    </>
+                </AdressView>
+                
                 <ScrollHorizontal horizontal={true} showsHorizontalScrollIndicator={false}>
 
-
-                <BtnFilter>
+                <BtnFilter style={{marginLeft: 15}}>
                     <>
                         <BtnText>Filtros</BtnText>
                         <Icon style={{marginLeft: 10}} name="tasks" size={12} />
@@ -62,11 +77,85 @@ export default () => {
                     <BtnText>Entrega Parceira</BtnText>
                 </BtnFilter>
 
-                 <BtnFilter>
+                 <BtnFilter style={{marginRight: 15}}>
                     <BtnText>Super Restaurante</BtnText>
                 </BtnFilter>
 
                 </ScrollHorizontal>
+
+                <ScrollImages horizontal={true} showsHorizontalScrollIndicator={false} decelerationRate="fast">
+                    <ImgView style={{marginLeft: 15}}>
+                        <Img source={require('../../assets/img/supermercado.jpg')} />
+                        <ImgTitle>Mercado</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/promoçoes.jpg')} />
+                        <ImgTitle>Promoções</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/vale_refeiçao.jpg')} />
+                        <ImgTitle>Vale-refeição</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/açai.jpg')} />
+                        <ImgTitle>Açai</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/doces_bolos.jpg')} />
+                        <ImgTitle>Doces & Bolos</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/salgados.jpg')} />
+                        <ImgTitle>Salgados</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/pastel.jpg')} />
+                        <ImgTitle>Pastel</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/sorvetes.jpg')} />
+                        <ImgTitle>Sorvetes</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/padarias.jpg')} />
+                        <ImgTitle>Padaria</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/brasileira.jpg')} />
+                        <ImgTitle>Brasileira</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/saudavel.jpg')} />
+                        <ImgTitle>Saudável</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/bebida.jpg')} />
+                        <ImgTitle>Bebidas</ImgTitle>
+                    </ImgView>
+
+                    <ImgView>
+                        <Img source={require('../../assets/img/pizza.jpg')} />
+                        <ImgTitle>Pizza</ImgTitle>
+                    </ImgView>
+
+                    <ImgView style={{marginRight: 15}}>
+                        <Img source={require('../../assets/img/vegetariana.jpg')} />
+                        <ImgTitle>Vegetariana</ImgTitle>
+                    </ImgView>
+                </ScrollImages>
+
+
             </Scroll>
         </Container>
     );
--- src/screens/HomeScreen/style.js
+++ src/screens/HomeScreen/style.js
@@ -5,23 +5,56 @@ export const Container = styled.SafeAreaView`
     background-color: #fff;
 `;
 
-export const Scroll = styled.ScrollView``;
+export const Scroll = styled.ScrollView`
+`;
+
+
+export const AdressView = styled.TouchableHighlight`
+    flex-direction: row;
+    align-items: center;
+    margin: 10px 0 0 15px;
+`;
+
+export const AdressText = styled.Text`
+    font-weight: bold;
+`;
 
 export const ScrollHorizontal = styled.ScrollView`
-    margin-top: 20px;
-    height: 100px;
+    margin-top: 32px;
+    height: 65px;
 `;
 
 export const BtnFilter = styled.TouchableHighlight`
-    height: 35px;
+    height: 30px;
     border-radius: 20px;
-    border: 1px solid #ccc;
+    border: 1px solid rgba(0, 0, 0, 0.1);
     align-items: center;
     justify-content: center;
     padding: 10px;
-    margin-left: 10px;
     flex-direction: row;
+    margin-left: 8px;
 `;
 export const BtnText = styled.Text`
-    color: #434343;
+    color: #888;
+`;
+
+
+export const ScrollImages = styled.ScrollView`
+`;
+
+export const ImgView = styled.View`
+    margin-left: 12px;
+    align-items: center;
+`;
+
+export const Img = styled.Image`
+    height: 70px;
+    width: 100px;
+    border-radius: 5px;
+`;
+
+export const ImgTitle = styled.Text`
+    font-size: 12px;
+    color: #888;
+    margin-top: 10px;
 `;
